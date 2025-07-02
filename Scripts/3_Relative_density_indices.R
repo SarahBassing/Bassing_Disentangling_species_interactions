fe17bbe1b16a4878cec802b2750f9a8956240cc5
SarahBassing <sarah.bassing@gmail.com>
2025-07-02 15:12:12 -0600

    Additional annotation about datasets, note about data availability & script transparency, clean up unnecessary code with anonymized data

--- Scripts/3_Relative_density_indices.R
+++ Scripts/3_Relative_density_indices.R
@@ -11,21 +11,36 @@
   #'  can be run independently but it is also sourced by 4_Structural_Equation_Models.R 
   #'  to create input data for competing SEMs.
   #'  
+  #'  SPATIALLY ANONYMIZED OUTPUTS FROM 2_CLUSTER_CAMERAS.R ARE PROVIDED TO ENSURE 
+  #'  CODE AND ANALYSIS ARE TRANSPARENT AND REPRODUCIBLE. CAMERA LOCATIONS USED 
+  #'  TO GENERATE THESE OUTPUTS WITH 2_CLUSTER_CAMERAS.R SCRIPT ARE SENSITIVE BUT 
+  #'  AVAILABLE UPON REQUEST FROM IDAHO DEPT OF FISH & GAME FOR QUALIFIED RESEARCHERS.
+  #'  
   #'  Requires: 
   #'    1. Relative abundance data (RN_abundance.RData) estimated by Royle-Nichols 
   #'    abundance model in 1_Relative_abundance_Royle-Nichols_model.R
-  #'    2. Spatial data for GMUs and clusters generated in 2_Cluster_cameras.R 
-  #'    3. Coveriate data housed in Data folder
+  #'    2. Cameras assigned to clusters generated in 2_Cluster_cameras.R housed in
+  #'    "./Data/" folder. Data include camera ID, season, GMU, and camera setup type, 
+  #'    plus GMU-specific cluster and area of cluster each camera was grouped in.
+  #'        1. stations_smr2020.RData
+  #'        2. stations_smr2021.RData
+  #'        3. stations_smr2022.RData 
+  #'    3. Shapefiles of cluster polygons generated in 2_Cluster_cameras.R housed 
+  #'    in "./Outputs/Spatial_outputs/" folder
+  #'        1. cluster_polygons_gmu1
+  #'        2. cluster_polygons_gmu6
+  #'        3. cluster_polygons.gmu10a 
+  #'    4. Covariate data housed in "./Data/" folder
   #'       Covariate data includes:
   #'        1. GMU: Game Management Unit where each camera cluster occurred
-  #'        2. ClusterID: Unique number of cluster within a GMU (ClusterID = 1 
-  #'        through n for clusters in each GMU)
+  #'        2. ClusterID: Unique number of clusters within a GMU 
+  #'        (ClusterID = 1 ... n for n clusters in each GMU)
   #'        3. Year: Year of data collection
   #'        4. DisturbedForest_last20Yrs: percent of cluster comprising forested 
   #'        habitat that was disturbed within the 20 years prior to year of data 
   #'        collection. Covariate generated using canopy loss data from the Global 
   #'        Forest Cover Change dataset extracted from Google Earth Engine. 
-  #'        5. annual harvest: total number of wolves harvested (trapped, hunted, 
+  #'        5. Annual harvest: total number of wolves harvested (trapped, hunted, 
   #'        or removed for management) reported within 1 year prior to start of 
   #'        annual data collection. Data provided by Idaho Department of Fish & Game.
   #'  --------------------------------
@@ -44,30 +59,26 @@
   #'  Load RN model local abundance estimates
   load("./Outputs/RN_abundance.RData")
   
-  #'  Function to clean up camera cluster spatial data
-  reformat_clusters <- function(clusters) {
-    skinny_clusters <- clusters %>%
-      dplyr::select(c(CamID, CellID, GMU, Setup, Clustrs, are_km2, geometry)) %>%
-      rename(ClusterID = Clustrs) %>%
-      rename(area_km2 = are_km2)
-    return(skinny_clusters)
-  }
-
-  #'  Read in camera cluster shapefiles and clean up with reformat_clusters() function
-  clusters_gmu1 <- st_read("./Outputs/Spatial_outputs/cam_clusters_gmu1.shp") %>%
-    reformat_clusters(.)
-  clusters_gmu6 <- st_read("./Outputs/Spatial_outputs/cam_clusters_gmu6.shp") %>%
-    reformat_clusters(.)
-  clusters_gmu10a <- st_read("./Outputs/Spatial_outputs/cam_clusters_gmu10a.shp") %>%
-    reformat_clusters(.)
-
-  #'  Merge spatial camera cluster data together and reduce to one observation per camera
-  clusters_all <- bind_rows(clusters_gmu1, clusters_gmu6, clusters_gmu10a) %>%
+  #'  Load camera station data, including assigned cluster (clusters based on output
+  #'  from 2_Cluster_cameras.R)
+  load("./Data/stations_smr2020.RData")
+  load("./Data/stations_smr2021.RData")
+  load("./Data/stations_smr2022.RData")
+  
+  #'  Merge camera cluster data together and reduce to one observation per camera
+  clusters_all <- bind_rows(stations_smr2020, stations_smr2021, stations_smr2022) %>%
     group_by(CamID) %>%
     slice(1L) %>%
-    ungroup()
+    ungroup() %>%
+    dplyr::select(-Season) %>%
+    #'  Change GMU and Setup to characters that match thos in RN_abundance
+    mutate(GMU = ifelse(GMU == 1, "GMU10A", GMU),
+           GMU = ifelse(GMU == 2, "GMU6", GMU),
+           GMU = ifelse(GMU == 3, "GMU1", GMU),
+           Setup = ifelse(Setup == 1, "U", "P"))
 
-  #'  Read in cluster polygon shapefiles
+  #'  Read in cluster polygon shapefiles (cluster polygons based on output from 
+  #'  2_Cluster_cameras.R)
   gmu1_poly <- st_read("./Outputs/Spatial_outputs/cluster_polygons_gmu1.shp") %>% mutate(GMU = "GMU1")
   gmu6_poly <- st_read("./Outputs/Spatial_outputs/cluster_polygons_gmu6.shp") %>% mutate(GMU = "GMU6")
   gmu10a_poly <- st_read("./Outputs/Spatial_outputs/cluster_polygons_gmu10a.shp") %>% mutate(GMU = "GMU10A")
@@ -75,13 +86,13 @@
   #'  Merge cluster polygons across GMUs
   cluster_poly <- bind_rows(gmu1_poly, gmu6_poly, gmu10a_poly) %>%
     rename(ClusterID = Clusters)
-  mapview::mapview(list(cluster_poly, clusters_all), zcol = "ClusterID")
+  mapview::mapview(cluster_poly, zcol = "ClusterID")
   
   #'  Load GMU spatial data
   gmu_wgs84 <- st_read("./Data/Spatial_data/Study_GMUs.shp") %>%
     st_transform("+proj=longlat +datum=WGS84 +no_defs")
   
-  #'  Load covariate data
+  #'  Load covariate data 
   load("./Data/covariates.RData")
   
   #'  ---------------------------------------------------
@@ -93,27 +104,27 @@
       left_join(clusters, by = c("CamID", "GMU", "Setup"))
     return(clustered_rai)
   }
-  RN_abundance_sf <- lapply(RN_abundance, cluster_RAI, clusters = clusters_all)
+  RN_abundance_cluster <- lapply(RN_abundance, cluster_RAI, clusters = clusters_all)
   
   #'  Calculate index of relative density per species per cluster per year
   density_per_cluster <- function(rai, yr) {
     relative_density <- rai %>%
-      group_by(GMU, ClusterID, Species) %>%
+      group_by(GMU, Clusters, Species) %>%
       reframe(SppN = sum(RN.n),
-              SppDensity.km2 = SppN/area_km2,
+              SppDensity.km2 = SppN/Area_km2,
               SppDensity.100km2 = SppDensity.km2*100,
               SppN.r = sum(round(RN.n, 0)),
-              SppDensity.km2.r = SppN.r/area_km2,
+              SppDensity.km2.r = SppN.r/Area_km2,
               SppDensity.100km2.r = SppDensity.km2.r*100) %>% 
       unique() %>%
       mutate(Year = yr) %>%
       #'  Join spatial polygon data to relative density estimates
-      left_join(cluster_poly, by = c("GMU", "ClusterID")) %>%
+      left_join(cluster_poly, by = c("GMU", "Clusters" = "ClusterID")) %>%
       st_as_sf()
     return(relative_density)
   }
   yr <- list("2020", "2021", "2022")
-  cluster_density <- mapply(density_per_cluster, rai = RN_abundance_sf, yr = yr, SIMPLIFY = FALSE)
+  cluster_density <- mapply(density_per_cluster, rai = RN_abundance_cluster, yr = yr, SIMPLIFY = FALSE)
   
   save(cluster_density, file = "./Outputs/RelativeDensityIndex_per_SppCluster.RData")
   
@@ -215,17 +226,17 @@
   #'  Long to wide data structure
   wide_data <- function(dat) {
     pivot_data_wide <- as.data.frame(dat) %>%
-      filter(!is.na(ClusterID)) %>%
+      filter(!is.na(Clusters)) %>%
       #'  Create categorical year variable
       mutate(Year = as.numeric(Year),
              year = ifelse(Year == 2020, "yr1", Year),
              year = ifelse(Year == 2021, "yr2", year),
              year = ifelse(Year == 2022, "yr3", year)) %>% 
-      dplyr::select(c(GMU, ClusterID, Year, year, Species, SppDensity.100km2.r)) %>%
+      dplyr::select(c(GMU, Clusters, Year, year, Species, SppDensity.100km2.r)) %>%
       #'  Create column per species with their site-specific local abundance 
       pivot_wider(names_from = "Species",
                   values_from = c("SppDensity.100km2.r")) %>%
-      left_join(covs, by = c("GMU", "Year", "ClusterID"))
+      left_join(covariates, by = c("GMU", "Year", "Clusters" = "ClusterID"))
     #'  Review to make sure it time lags look right given these observations are 
     #'  hypothesized to affect the next set of observations
     print(pivot_data_wide)
@@ -265,7 +276,6 @@
   #'  ----------------------------------------------
   #'  Grouping data across years so t-1 data affects t data, regardless of year,
   #'  increases sample size and eliminates annual variation in estimated relationships
-  #'  but prohibits estimation of indirect effects (yr 1 & 2 affect yr 2 & 3... yr2 on both sides)
   #'  Stack year 1 & year 2 data
   dat_t_minus_1 <- dat_stack_list[[1]] %>% 
     #'  Indicate whether observation was from first or second year in grouped data
@@ -282,11 +292,11 @@
     dplyr::select(-year)
   
   #'  Join t-1 and t data based on camera location
-  density_wide_1YrLag_20s_22s <- full_join(dat_t_minus_1, dat_t, by = c("GMU", "ClusterID", "GroupYear")) %>%
-    relocate(GroupYear, .after = ClusterID) %>%
+  density_wide_1YrLag_20s_22s <- full_join(dat_t_minus_1, dat_t, by = c("GMU", "Clusters", "GroupYear")) %>%
+    relocate(GroupYear, .after = Clusters) %>%
     #'  Drop sites with NAs (missing 1+ years of data)
     na.omit(.) %>%
-    mutate(ClusterID = as.character(ClusterID),
+    mutate(Clusters = as.character(Clusters),
            Year.x = as.character(Year.x),
            Year.y = as.character(Year.y)) 
   
@@ -301,7 +311,7 @@
     cor_matrix <- cor(covs, use = "complete.obs")
     return(cor_matrix)
   }
-  cov_correlation(localN_z_1YrLag) #'  Annual prey N correlated across years
+  cov_correlation(localN_z_1YrLag) 
   
   #'  Visualize data
   plot_histograms <- function(dat) {
@@ -311,21 +321,6 @@
     }
   }
   plot_histograms(localN_z_1YrLag)
-  
-  #'  Check out a few basic relationships
-  summary(lm(whitetailed_deer.T ~ mountain_lion.Tminus1 + wolf.Tminus1 + coyote.Tminus1, data = localN_z_1YrLag))  
-  summary(lm(elk.T ~ mountain_lion.Tminus1 + wolf.Tminus1 + bear_black.Tminus1, data = localN_z_1YrLag))  
-  summary(lm(moose.T ~ wolf.Tminus1, data = localN_z_1YrLag))
-  summary(lm(bear_black.T ~ wolf.Tminus1 + mountain_lion.T, data = localN_z_1YrLag))
-  summary(lm(mountain_lion.T ~ wolf.Tminus1 + bear_black.Tminus1, data = localN_z_1YrLag))
-  summary(lm(coyote.T ~ wolf.Tminus1 + mountain_lion.Tminus1, data = localN_z_1YrLag))
-  summary(lm(wolf.T ~ wolf.Tminus1 + annual_harvest.Tminus1, data = localN_z_1YrLag))
-  
-  plot(whitetailed_deer.T ~ mountain_lion.Tminus1, data = localN_z_1YrLag)
-  plot(elk.T ~ mountain_lion.Tminus1, data = localN_z_1YrLag)
-  plot(whitetailed_deer.T ~ wolf.Tminus1, data = localN_z_1YrLag)
-  plot(moose.T ~ wolf.Tminus1, data = localN_z_1YrLag)
-  plot(coyote.T ~ wolf.Tminus1, data = localN_z_1YrLag)
   
   #'  Save outputs
   save(density_wide_1YrLag_20s_22s, file = "./Outputs/data_for_SEM_1YrLag_n.RData")
